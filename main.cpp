8badc39d7c680c61f9ecd90f46f082493a921cf8
Gabriel Eller <51245361+ellergabriel@users.noreply.github.com>
2022-05-14 23:21:12 +0000

    basic set up

--- main.cpp
+++ main.cpp
@@ -1,5 +1,67 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+//========
+// Gabriel Eller 
+//========
+
+#define NUM_RINGS  3
+#define NUM_PEGS  3
+#define L_WEIGHT  2.5
+#define M_WEIGHT  1
+#define S_WEIGHT  .5
+//#define TOTAL_WEIGHT 4
+
+
+//helper class to store each state of the puzzle
+class State{
+  public:
+    State(){}
+  
+    State(float lastPegs[]){
+      for(int i = 0; i < NUM_PEGS; i++){
+        pegs[i] = lastPegs[i];
+      }
+    }
+    
+    float h(){
+      return 3 - pegs[NUM_PEGS - 1];
+    }
+  
+    float f(){
+      return h() + g;
+    }
+  
+    float* getPegs(){
+      return pegs;
+    }
+
+
+    int g = 0;
+  
+    float pegs[];
+};
+
+bool isLegalMove(State state, int peg, int disk){
+  //either peg has larger disks already in place, no disks in place
+  //or fringe case of MSL, where weight returns true despite being illegal
+  return ( (state.getPegs()[peg] > disk && state.getPegs()[peg] != L_WEIGHT + S_WEIGHT)
+           || state.getPegs()[peg] == 0);
+}
 
 int main() {
-  //std::cout << "Hello World!\n";
+  bool isSolved = false;
+  float g, h ,f, eval;
+  g = h = f = eval = 0;
+  float pegs[NUM_PEGS];
+  //initialize pegs to SML[empty][empty]
+  //pegs[0] = TOTAL_WEIGHT;
+
+  vector<State> frontier;
+  while(!isSolved){
+    for(int i = 0; i < frontier.size(); i++){ 
+      eval = (eval > frontier[i].f()) ? frontier[i].f() : eval;
+    }
+  }
 }
