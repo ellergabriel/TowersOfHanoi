ab07818f2bcdb36e0650cc8218280ba24ed9cc48
Gabriel Eller <51245361+ellergabriel@users.noreply.github.com>
2022-05-15 19:31:09 +0000

    weird bug

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 using namespace std;
 
 //========
@@ -11,57 +12,109 @@ using namespace std;
 #define L_WEIGHT  2.5
 #define M_WEIGHT  1
 #define S_WEIGHT  .5
+
+string S_STRING = "  S    ";
+string M_STRING = " MMM   ";
+string L_STRING = "LLLLL  ";
 //#define TOTAL_WEIGHT 4
 
+float convertChar(char c){
+  switch(c){
+    case 'S':
+      return S_WEIGHT;
+    case 'M':
+      return M_WEIGHT;
+    case 'L':
+      return L_WEIGHT;
+    default:
+      return 0;
+  }
+}
 
 //helper class to store each state of the puzzle
 class State{
   public:
-    State(){}
-  
-    State(float lastPegs[]){
-      for(int i = 0; i < NUM_PEGS; i++){
-        pegs[i] = lastPegs[i];
+    State(){
+      pegs.push_back(vector<char> (3, 0));
+      pegs.push_back(vector<char> (3, 0));
+      pegs.push_back(vector<char> (3, 0));
+      pegs[0].push_back('S');
+      pegs[0].push_back('M');
+      pegs[0].push_back('L');
+    }
+
+    State(State* origin){
+      pegs = origin->pegs;
+      g = origin->g;
+    }
+
+    bool isLegalMove(int dest, char disk){
+      return (convertChar(disk) < convertChar(pegs[dest].front()));
+    }
+
+    bool moveDisk(int origin, int dest, char disk){
+      if(isLegalMove(dest, disk)){
+        pegs[origin].erase(pegs[origin].begin());
+        pegs[dest].emplace(pegs[dest].begin(), disk);
+        g++;
+        return true;
       }
+      return false;
     }
-    
+
     float h(){
-      return 3 - pegs[NUM_PEGS - 1];
+      float val = 0;
+      for(int i = 0; i < pegs[NUM_PEGS - 1].size(); i++){
+        val += convertChar(pegs[NUM_PEGS - 1][i]);
+      }
+      return 3 - val;
     }
-  
+        
     float f(){
       return h() + g;
     }
-  
-    float* getPegs(){
-      return pegs;
-    }
-
 
     int g = 0;
-  
-    float pegs[];
+    vector< vector<char> > pegs;
 };
 
-bool isLegalMove(State state, int peg, int disk){
-  //either peg has larger disks already in place, no disks in place
-  //or fringe case of MSL, where weight returns true despite being illegal
-  return ( (state.getPegs()[peg] > disk && state.getPegs()[peg] != L_WEIGHT + S_WEIGHT)
-           || state.getPegs()[peg] == 0);
+static void printState(State* s){
+  int level = 3;
+  char c = ' ';
+  string empty = "  |    ";
+  for(int i = 0; i < NUM_PEGS; i++){
+    for(int j = 0; j < NUM_RINGS; j++){
+      level = 3 - i;
+      c = s->pegs[i][j];
+      if(s->pegs[i].size() >= level){
+        switch (s->pegs[i][j]){
+          case 'S':
+            cout << S_STRING;
+            break;
+          case 'M':
+            cout << M_STRING;
+            break;
+          case 'L':
+            cout << L_STRING;
+            break;
+          default: 
+            break;
+        }
+      } else {
+        cout << empty;
+      }
+    }
+  }
 }
 
 int main() {
-  bool isSolved = false;
-  float g, h ,f, eval;
-  g = h = f = eval = 0;
-  float pegs[NUM_PEGS];
+  float eval;
+  eval = 0;
   //initialize pegs to SML[empty][empty]
   //pegs[0] = TOTAL_WEIGHT;
 
   vector<State> frontier;
-  while(!isSolved){
-    for(int i = 0; i < frontier.size(); i++){ 
-      eval = (eval > frontier[i].f()) ? frontier[i].f() : eval;
-    }
-  }
+  State start = new State();
+  printState(&start);
+  return 0;
 }
